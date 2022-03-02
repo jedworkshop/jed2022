a4dc32675fab50044b92baae3d71be644f529a55
Sorami Hisamoto <sorami.hisamoto@legalscape.co.jp>
2022-03-02 22:09:33 +0900

    「全ての概要を開く/閉じる」ボタンを追加

--- src/js/collapsible.js
+++ src/js/collapsible.js
@@ -11,3 +11,26 @@ window.addEventListener('click', (event) => {
     target.classList['toggle']('show');
   }
 }, false);
+
+
+const allButton = document.querySelector('[class="btn_abstract_all"]');
+allButton.addEventListener('click', (event) => {
+  const element = event.target;
+  const selector = element.getAttribute('data-target');
+
+  const action = element.getAttribute('data-action');
+  console.log(action);
+  if (action === 'open') {
+    document.querySelectorAll(selector).forEach(target => {
+      target.classList['add']('show');
+      element.setAttribute('data-action', 'close');
+      element.innerHTML = '全ての概要を閉じる';
+    });
+  } else {
+    document.querySelectorAll(selector).forEach(target => {
+      target.classList['remove']('show');
+      element.setAttribute('data-action', 'open');
+      element.innerHTML = '全ての概要を開く';
+    });
+  }
+}, false);
